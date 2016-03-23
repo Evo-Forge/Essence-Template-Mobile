f20409657113b0c0ebb27149d8a86d8cd83d54f0
Mihai Chiorean <mihay_55@yahoo.com>
2016-03-23 10:40:13 +0200

    create payment js and less file

---
+++ js/components/payment.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ClassNames from 'classnames';
+
+import { Block, Btn, Icon, Image, Text, Utils} from 'react-essence';
+import Input from 'essence-input';
+
+
+
+class MobileConfirmation extends React.Component {
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+module.exports = MobileConfirmation;
