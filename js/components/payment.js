286726f14c861c7aade778631837b4b1113565f8
Mihai Chiorean <mihay_55@yahoo.com>
2016-03-23 14:53:52 +0200

    razvan take from here

--- js/components/payment.js
+++ js/components/payment.js
@@ -1,33 +1,55 @@
 import React from 'react';
 import ClassNames from 'classnames';
 
-import { Btn, AppBar, Block, Image,  Input, Navigation, Tab, Text, Utils, Menu, List, ListItem} from 'react-essence';
+import { Btn, AppBar, Block, Image, Icon, Input, Navigation, Tab, Text, Utils, Menu, List, ListItem} from 'react-essence';
 
 var tabs = {
  'header': [
-   { 'context': (<Text>SIGN UP</Text>) },
-   { 'context': (<Text>SIGN IN</Text>) }
+   { 'context': (<Text className={'e-text-grey-600'}>CREDIT CARD</Text>) },
+   { 'context': (<Text className={'e-text-grey-600'}>PAYPAL</Text>) }
   ],
   'rows': [
-   ( <Block>
-     <Block classes={'e-row relative e-no-margin div-height  align-center'}>
-      <Block classes={'brick brick-12'}>
-        <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>USERNAME</Text>
-        <Input type={'text'} name={'label'} label={'Input with label'} />
-      </Block>
-      <Block classes={'brick brick-12'}>
-        <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>PASSWORD</Text>
-        <Input type={'email'} name={'label'} label={'Email input'} />
-      </Block>
-      <Block classes={'brick brick-12 absolute-text'}>
-        <Text className={'e-text-grey-400 brick brick-12'}>
-          Dont have an Account<Text type={'a'} className={'e-text-grey-800'}> Sign Up</Text>
-        </Text>
-      </Block>
-    </Block>
-    <Block className={'e-no-margin button-div fixed-bottom'}>
-      <Btn label={'Sign In'} ripple={true} className={'flat e-background-teal-A700 adjust-button e-no-margin e-text-white'} />
-    </Block>
+   ( <Block className={'e-no-padding'}>
+       <Block classes={'mobile-cart'}>
+          <Block className={'cart-item'}>
+            <Block className={'e-background-grey-100'}>
+              <Text type={'p'} classes={'e-text-center e-button'} className={'e-no-margin'}>total</Text>
+              <Text type={'h1'} classes={'e-text-center e-headline e-no-margin'}>
+                <Icon name={"editor-attach-money"} />
+                22.00
+              </Text>
+            </Block>
+            <Block classes={'relative e-no-margin div-height e-row align-top'}>
+             <Block classes={'brick brick-6 adjust-height-input'}>
+               <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>FULL NAME</Text>
+               <Input type={'text'} name={'label'} label={'Input with label'} />
+             </Block>
+             <Block classes={'brick brick-6 adjust-height-input'}>
+               <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>EMAIL</Text>
+               <Input type={'email'} name={'label'} label={'Email input'} />
+             </Block>
+             <Block classes={'brick brick-8 adjust-height-input'}>
+               <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>PASSWORD</Text>
+               <Input type={'text'} name={'label'} label={'Input with label'} />
+             </Block>
+             <Block classes={'brick brick-5 adjust-height-input'}>
+               <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>TELEPHONE NUMBER</Text>
+               <Input type={'text'} name={'label'} label={'Input with label'} />
+             </Block>
+             <Block classes={'brick brick-6 adjust-height-input'}>
+               <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>FULL NAME</Text>
+               <Input type={'text'} name={'label'} label={'Input with label'} />
+             </Block>
+             <Block classes={'brick brick-6 adjust-height-input'}>
+               <Text type={'span'} classes={'e-text-left e-body1 e-text-grey-400'}>EMAIL</Text>
+               <Input type={'email'} name={'label'} label={'Email input'} />
+             </Block>
+            </Block>
+          </Block>
+       </Block>
+       <Block className={'e-no-margin button-div fixed-bottom'}>
+        <Btn label={'Place Order'} ripple={true} className={'flat e-background-teal-A700 adjust-button e-no-margin e-text-white'} />
+       </Block>
     </Block>
 ),
 (<Block>
@@ -54,7 +76,7 @@ var tabs = {
      </Text>
    </Block>
    <Block className={'e-no-margin button-div fixed-bottom'}>
-     <Btn label={'Create Account'} ripple={true} className={'flat e-background-teal-A700 adjust-button e-no-margin e-text-white'} />
+     <Btn label={'Place Order'} ripple={true} className={'flat e-background-teal-A700 adjust-button e-no-margin e-text-white'} />
    </Block>
  </Block>
  </Block>
